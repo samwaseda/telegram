302ae23bf4e632406b74d3ab29113d94cdd24848
samwaseda <o.waseda@mpie.de>
2024-12-16 21:07:21 +0000

    final version

--- telegram/secret_santa/wichtel_bot.py
+++ telegram/secret_santa/wichtel_bot.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from hashlib import sha256
 
 with open("BOT_API", "r") as f:
     TELEGRAM_TOKEN = f.read().split("\n")[0]
@@ -8,7 +9,18 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def get_review(message):
-    bot.send_message(message.chat.id, message.text)
+    hash_dict = {}
+    with open("hash_list.txt", "r") as f:
+        hash_list = f.read().split("\n")
+        for h in hash_list:
+            h_split = h.split()
+            hash_dict[h_split[0]] = h_split[1]
+    key = sha256(message.text.split()[1].encode()).hexdigest()
+    name = hash_dict.pop(key, None)
+    bot.send_message(message.chat.id, "You give a present to: " + name)
+    with open("hash_list.txt", "w") as f:
+        for k, v in hash_dict.items():
+            f.write(k + " " + v + "\n")
 
 
 bot.infinity_polling()
